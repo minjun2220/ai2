49ce9052e538e05951192f258ab61e1f1e415b2e
minjun2220 <20242220@sdhs.gwe.hs.kr>
2024-11-20 11:30:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1NKIhMhUeRC0vPptHwT4it-LMYhamVDyi'
+file_id = '1K0Q19onHJjbMuQ8hSpIObeKZvZoP11MP'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
@@ -52,11 +52,11 @@ if uploaded_file is not None:
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
     if prediction == labels[0]:
-         st.write("중국식 냉면은 중국이 아니라 한국에서 만듬")
+         st.write("a")
     elif prediction == labels[1]:
-         st.write("짜장면은 굿")
+         st.write("b")
     elif prediction == labels[2]:
-         st.write("짬뽕은 맵지만 맛있어!!")
+         st.write("c")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
